2d328a8df75f7c9d91938d5de364d03e270a45d7
RaulToledoProcopio <rtolpro@gmail.com>
2024-12-16 01:00:06 +0100

    Depuración

--- src/main/kotlin/com/es/gameStore/ServletInitializer.kt
+++ src/main/kotlin/com/es/gameStore/ServletInitializer.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 class ServletInitializer : SpringBootServletInitializer() {
 
 	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-		return application.sources(JwtSecurityKotlinApplication::class.java)
+		return application.sources(GameStoreApplication::class.java)
 	}
-
 }
